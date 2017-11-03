016b0cf6860b0c6287b6327ec74058b7c6260485
Robert Kirkum <robert113289@gmail.com>
2017-11-03 11:17:04 -0500

    completed class3.2 ListCount

---
+++ Class3_2_ListCount/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Class3_2_ListCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int result = 0;
+            
+            foreach (int number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
