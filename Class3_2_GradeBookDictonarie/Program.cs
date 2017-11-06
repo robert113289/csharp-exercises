cf9c3769865d3111840eb0482454d3a8d5ce7156
Robert Kirkum <robert113289@gmail.com>
2017-11-06 15:21:01 -0600

    finished class 3.2 exercises.

---
+++ Class3_2_GradeBookDictonarie/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class3_2_GradeBookDictonarie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> GradeBook = new Dictionary<int, string>();
+            int Id;
+            string Name;
+            string input;
+
+
+
+            do
+            {
+                Console.WriteLine("Enter student ID:");
+                input = Console.ReadLine();
+                
+
+                if (input != "")
+                {
+                    Id = int.Parse(input);
+                    Console.WriteLine("Enter student name:");
+                    input = Console.ReadLine();
+                    Name = input;
+
+                    GradeBook.Add(Id, Name);
+                }
+
+
+            }
+            while (input != "");
+
+
+            //print roster
+
+            //           foreach (KeyValuePair<int,string> student in GradeBook)
+            //         {
+            //           Console.WriteLine("Id=" + student.Key + " (" + student.Value + ")");
+            //     }
+
+            // Print class roster using string builder
+            Console.WriteLine("\nClass roster:");
+            StringBuilder MyStringBuilder = new StringBuilder("Student=");
+            foreach (KeyValuePair<int, string> student in GradeBook)
+            {
+                MyStringBuilder.Append(student);
+                
+            }
+            Console.WriteLine(MyStringBuilder);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
--- Class3_2_ListCount/Program.cs
+++ Class3_2_ListCount/Program.cs
@@ -9,15 +9,27 @@ namespace Class3_2_ListCount
         static void Main(string[] args)
         {
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int result = 0;
             
-            foreach (int number in numbers)
-            {
-                result += number;
-            }
 
-            Console.WriteLine(result);
+            Console.WriteLine(Sum.EvenSum(numbers));
             Console.ReadLine();
+
+        } 
+    }
+
+    class Sum
+    {
+        public static int EvenSum (List<int> numbers)
+
+        {
+            int result = 0;
+
+            foreach (int number in numbers)
+                if (number % 2 == 0)
+                {
+                    result += number;
+                }
+            return result;
         }
     }
 }
