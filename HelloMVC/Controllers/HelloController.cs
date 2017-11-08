d06057f7c030ab993152172ccfda6be79bb10860
Robert Kirkum <robert113289@gmail.com>
2017-11-08 12:49:06 -0600

    created HelloMVC

---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+
+        }
+    }
+}
